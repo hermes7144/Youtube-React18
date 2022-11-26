1267033ea820490c8aeb0f0638fb74b02ac265cb
hermes7144 <kichancho@gmail.com>
2022-11-26 15:43:23 +0900

    youtube 비디오 페이지 구현, api데이터 받아오기

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { formatAgo } from '../utils/date';
+
+export default function VideoCard({ video }) {
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <li>
+      <img className="w-full" src={thumbnails.medium.url} alt={title} />
+      <p className="font-semibold my-2 line-clamp-2">{title}</p>
+      <p className="text-sm opacity-80">{channelTitle}</p>
+      <p className="text-sm opacity-80">{formatAgo(publishedAt, 'ko')}</p>
+    </li>
+  );
+}
